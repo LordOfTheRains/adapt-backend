19c1f5e49dae3e1bffa1884b58ede9a2f05bfe30
rkv0004 <37938431+rkv0004@users.noreply.github.com>
2019-01-30 10:01:22 -0600

    Delete create-lb-tables.js

--- create-lb-tables.js
+++
@@ -1,8 +0,0 @@
-var server = require('./server');
-var ds = server.dataSources.mysqlD;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Residents', 'Recommendations', 'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips','SavedHouseRecommendations','ResidentConcerns','ResidentHouses'];
-ds.automigrate(lbTables, function(er) {
-  if (er) throw er;
-  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-  ds.disconnect();
-});

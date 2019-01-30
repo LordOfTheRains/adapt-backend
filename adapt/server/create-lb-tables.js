84d37db611d8903b8967eea5b8eaf303d56102ee
rkv0004 <37938431+rkv0004@users.noreply.github.com>
2019-01-30 10:16:36 -0600

    Update create-lb-tables.js
    Change line 2 to use your connector

--- adapt/server/create-lb-tables.js
+++ adapt/server/create-lb-tables.js
@@ -1,5 +1,5 @@
 var server = require('./server');
-var ds = server.dataSources.mysqlD;
+var ds = server.dataSources.mysqlD; //change the last part to the corresponding connector you are using
 var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Residents', 'Recommendations', 'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips','SavedHouseRecommendations','ResidentConcerns','ResidentHouses'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;

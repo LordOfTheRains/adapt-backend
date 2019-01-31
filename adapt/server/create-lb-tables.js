4789640d721103efc55a0015c40cc21709874b6b
rkv0004 <37938431+rkv0004@users.noreply.github.com>
2019-01-30 20:04:05 -0600

    Model Relations Done? and Connected to a online database
    I haven't tested all the relations to make sure that they work as intended. I am going to do that tomorrow. The only relation I am not sure will work is finding tips. Also, I tested the connection to the online database.

--- adapt/server/create-lb-tables.js
+++ adapt/server/create-lb-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server');
-var ds = server.dataSources.mysqlD; //change the last part to the corresponding connector you are using
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Residents', 'Recommendations', 'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips','SavedHouseRecommendations','ResidentConcerns','ResidentHouses'];
+var ds = server.dataSources.mysql;//change the last part to whatever datasource connector name you are using
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Residents', 'Recommendations', 'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips','SavedHouseRecommendations','ResidentConcerns','ResidentHouses', 'ResidentRoomConcerns'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);

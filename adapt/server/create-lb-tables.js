76de127cf0e0c0dec33c3c46fc9e2920eecae7e5
Rain Li <yrl0001@auburn.edu>
2019-02-02 11:30:13 -0600

    added backend model

--- adapt/server/create-lb-tables.js
+++ adapt/server/create-lb-tables.js
@@ -1,6 +1,11 @@
 var server = require('./server');
-var ds = server.dataSources.mysqlD;//change the last part to whatever datasource connector name you are using
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Residents', 'Recommendations', 'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips','SavedHouseRecommendations','ResidentConcerns','ResidentHouses', 'ResidentRoomConcerns', 'Website', "RecommendationWebSites","TipsWebsites"];
+var ds = server.dataSources.adapt-db;//change the last part to whatever datasource connector name you are using
+var lbTables = ['User', 'AccessToken', 'ACL',
+  'RoleMapping', 'Role','Residents', 'Recommendations',
+  'Tips', 'Houses', 'PhysicalConcerns', 'Rooms','SavedHouseTips',
+  'SavedHouseRecommendations','ResidentConcerns','ResidentHouses',
+  'ResidentRoomConcerns', 'Website', "RecommendationWebSites",
+  'TipsWebsites'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);

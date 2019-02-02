92b95746da5e964b2dcb771a84f1823462a48911
Rain Li <yrl0001@auburn.edu>
2019-02-02 11:43:50 -0600

    updated path again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ git pull https://github.com/LordOfTheRains/adapt-backend.git
 echo "changing into app directory"
 cd adapt
 sudo npm install
-echo "migratijng models from loopback to database"
+echo "migrating models from loopback to database"
 node ./server/create-lb-tables.js
 echo "running adapt on local server"
 node .

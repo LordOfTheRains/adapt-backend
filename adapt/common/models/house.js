c9a7c11a30a1b7e8e2e3fb00814dbce3fd4d9057
Rain Li <yrl0001@auburn.edu>
2019-02-14 00:23:53 -0600

    WIP getting house

--- adapt/common/models/house.js
+++ adapt/common/models/house.js
@@ -1,5 +1,42 @@
 'use strict';
 
-module.exports = function(house) {
+module.exports = function(House) {
+  House.observe('before save',async function(ctx) {
+    var houseInstance = ctx.instance;
+    //get the residents list for this house
+    var residents = houseInstance.residents;
+    houseInstance.numResident = residents.length;
+    return;
+  });
+
+  //go through all residents of the house and
+  House.getFilterableCategories = function(house) {
+    var categories = {
+      "incomes":[],
+      "ages":[],
+      "genders":[],
+      "locations":[],
+      "concerns":[]
+    }
+    var incomes = [];
+    var ages = [];
+    var genders = [];
+    var locations = [];
+    var concerns = [];
+    house.residents.forEach(function(resident){
+      categories.incomes.append(resident.income);
+      categories.ages.append(resident.age);
+      categories.genders.append(resident.gender);
+      categories.locations.append(resident.location);
+      //concerns.append(resident.income);
+    });
+    categories.incomes = categories.incomes.unique();
+    categories.ages = categories.ages.unique();
+    categories.genders = categories.genders.unique();
+    categories.locations = categories.locations.unique();
+    return categories;
+
+  }
+
 
 };
--- adapt/common/models/room.js
+++ adapt/common/models/room.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const LBFilter = require('loopback-filters');
 module.exports = function(Room) {
 
   Room.remoteMethod('getRecommendations',{
@@ -11,20 +11,32 @@ module.exports = function(Room) {
   Room.getRecommendations = function(roomId, cb) {
     //https://loopback.io/doc/en/lb3/Querying-data.html
     var app = Room.app;
-    var genders = [0,1]//this can be retrieved from roomID
-    var roomTypes = [0,1,2]//this can be retrieved from roomID
-    var recommendations = app.models.Recommendation;
-    let filter={
-                  where: {
-                    and:[
-                      {applicableRoomTypes: roomTypes},
-                      {applicableGenders: genders}
-                    ]
-                  }
-              }
-    var filtered = require('loopback-filters')(recommendations.get(), filter);
-    console.log(filtered); // => [{n: 3}]
-    cb(null, filtered)
+    return Room.findById(roomId,[{include: {relation: 'house'}}], function(err, room){
+        console.log(roomId);
+        if (err == null && room != null) {
+          var r = room.toJSON();
+          var categories = app.models.House.getFilterableCategories(r.house);
+          var genders = [0,1]//this can be retrieved from roomID
+          var roomTypes = [0,1,2]//this can be retrieved from roomID
+          var recommendations = app.models.Recommendation;
+          let filter={
+                        where: {
+                          and:[
+                            {applicableRoomTypes: roomTypes},
+                            {applicableGenders: genders}
+                          ]
+                        }
+                    }
+          return recommendations.find(function(err,recs){
+            var filtered = LBFilter(recs, filter);
+            return filtered;
+          });
+
+        }else{
+          console.log(err);
+        }
+    });
+
   }
 
 

d4f51b7ac1a0109ea2f887419fe1b7f5852d8bb6
Rain Li <yrl0001@auburn.edu>
2019-02-26 22:48:55 -0600

    middle of filter  testing

--- adapt/common/models/house.js
+++ adapt/common/models/house.js
@@ -1,40 +1,48 @@
 'use strict';
+var uniq = require('lodash.uniq');//unique eleemnts in array
 
 module.exports = function(House) {
   House.observe('before save',async function(ctx) {
     var houseInstance = ctx.instance;
     //get the residents list for this house
     var residents = houseInstance.residents;
+    console.log(residents);
     houseInstance.numResident = residents.length;
     return;
   });
 
   //go through all residents of the house and
-  House.getFilterableCategories = function(house) {
-    var categories = {
-      "incomes":[],
-      "ages":[],
-      "genders":[],
-      "locations":[],
-      "concerns":[]
-    }
-    var incomes = [];
-    var ages = [];
-    var genders = [];
-    var locations = [];
-    var concerns = [];
-    house.residents.forEach(function(resident){
-      categories.incomes.append(resident.income);
-      categories.ages.append(resident.age);
-      categories.genders.append(resident.gender);
-      categories.locations.append(resident.location);
-      //concerns.append(resident.income);
+  House.getFilterableCategories = function(houseID, cb) {
+    return House.findById(houseID,{include: {relation: 'residents'}}, function(err, house){
+
+      var categories = {
+        "incomes":[],
+        "ages":[],
+        "genders":[],
+        "locations":[],
+        "concerns":[]
+      }
+      if (house == null){
+        return categories;
+      }
+      var incomes = [];
+      var ages = [];
+      var genders = [];
+      var locations = [];
+      var concerns = [];
+      house.toJSON().residents.forEach(function(resident){
+        categories.incomes.push(resident.income);
+        categories.ages.push(resident.age);
+        categories.genders.push(resident.gender);
+        categories.locations.push(resident.location);
+      });
+      categories.incomes = uniq(categories.incomes);
+      categories.ages = uniq(categories.ages);
+      categories.genders = uniq(categories.genders);
+      categories.locations = uniq(categories.locations);
+
+      cb(null, categories);
     });
-    categories.incomes = categories.incomes.unique();
-    categories.ages = categories.ages.unique();
-    categories.genders = categories.genders.unique();
-    categories.locations = categories.locations.unique();
-    return categories;
 
   }
 
--- adapt/common/models/room.js
+++ adapt/common/models/room.js
@@ -10,30 +10,42 @@ module.exports = function(Room) {
 
   Room.getRecommendations = function(roomId, cb) {
     //https://loopback.io/doc/en/lb3/Querying-data.html
-    var app = Room.app;
-    return Room.findById(roomId,[{include: {relation: 'house'}}], function(err, room){
-        console.log(roomId);
-        if (err == null && room != null) {
-          var r = room.toJSON();
-          var categories = app.models.House.getFilterableCategories(r.house);
-          var genders = [0,1]//this can be retrieved from roomID
-          var roomTypes = [0,1,2]//this can be retrieved from roomID
-          var recommendations = app.models.Recommendation;
-          let filter={
-                        where: {
-                          and:[
-                            {applicableRoomTypes: roomTypes},
-                            {applicableGenders: genders}
-                          ]
-                        }
-                    }
-          return recommendations.find(function(err,recs){
-            var filtered = LBFilter(recs, filter);
-            return filtered;
-          });
+    const requestPromise = new Promise((resolve, reject) => {
 
+
+    });
+    var app = Room.app;
+    return Room.findById(roomId,{include: {relation: 'house'}}, function(err, room){
+        if (err == null && room != null) {
+          var thisRoom = room.toJSON();
+          app.models.House.getFilterableCategories(thisRoom.houseId, function(err, categories){
+            var Recommendation = app.models.Recommendation;
+            let filter={
+                        "where": {
+                          "applicableGenders": {"inq": categories.genders}
+                        }
+                      }
+            console.log(categories.genders);
+            // Recommendation.find({"where":
+            //   "applicableGenders": {"inq": [0]}}
+            // }, function(err, recs){
+            //
+            //   console.log(recs);
+            //   cb(null,recs);
+            // })
+
+
+            Recommendation.find({where: {applicableGenders: {inq: [1]}}}, function(err,recs){
+              var filtered = LBFilter(recs, filter);
+              console.log(recs);
+              console.log(filter);
+              console.log(filtered);
+              cb(null,filtered);
+            });
+          })
         }else{
           console.log(err);
+          cb(err,[]);
         }
     });
 

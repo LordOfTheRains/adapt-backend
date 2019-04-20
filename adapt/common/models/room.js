824ba52eaaa99dd6ac21f4463195df8572519014
Rain Li <yrl0001@auburn.edu>
2019-04-20 11:09:24 -0500

    updated get recommendations filter api and other related model field modificaitron

--- adapt/common/models/room.js
+++ adapt/common/models/room.js
@@ -2,40 +2,6 @@
 const LBFilter = require('loopback-filters');
 module.exports = function(Room) {
 
-  Room.remoteMethod('getRecommendations',{
-    accepts: [{arg: 'id', type: 'number', required: true}],
-    http: {path: '/:id/getRecommendations', verb: 'get'},
-    returns: { type: ['object'], root: true }
-  });
-
-  Room.getRecommendations = function(roomId, cb) {
-    //https://loopback.io/doc/en/lb3/Querying-data.html
-    const requestPromise = new Promise((resolve, reject) => {
-
-
-    });
-    var app = Room.app;
-    return Room.findById(roomId,{include: {relation: 'house'}}, function(err, room){
-        if (err == null && room != null) {
-          var thisRoom = room.toJSON();
-          app.models.House.getFilterableCategories(thisRoom.houseId, function(err, categories){
-            var Recommendation = app.models.Recommendation;
-            console.log(categories.genders);
-
-
-            Recommendation.find(function(err,recs){
-              
-              cb(null,recs);
-            });
-          })
-        }else{
-          console.log(err);
-          cb(err,[]);
-        }
-    });
-
-  }
-
 
 
 };
---
+++ adapt/create-lb-tables.js
@@ -0,0 +1,79 @@
+var server = require('./server/server');
+var ds = server.dataSources.adaptDB;//change the last part to whatever datasource connector name you are using
+
+var app = server;
+var Room = app.models.Room;
+var House = app.models.House;
+var Resident = app.models.Resident;
+
+var lbTables = ['AccessToken', 'ACL','RoleMapping', 'Role',
+'User', 'House', 'Room',"Resident", "Recommendation", "Tip"];
+
+//https://loopback.io/doc/en/lb3/Implementing-auto-migration.html
+//automigrate recreates the whole database.
+//autoupdte detects differences.
+
+const shouldCreateDummyData = true;
+ds.automigrate().then(function(er) {
+  console.log('Loopback tables [' - lbTables - '] migrate completed for', ds.adapter.name);
+  if (er) throw er;
+  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  if (!shouldCreateDummyData) {
+    return;
+  }
+  //create sample data
+  House.create([
+    {
+      "name": "h1",
+      "numResident": 0,
+      "userId": 0
+    },
+    {
+      "name": "h2",
+      "numResident": 0,
+      "userId": 0
+    }
+  ], function(err, houses){
+    if (er) throw er;
+    Room.create([
+      {
+        "name": "r1",
+        "description": "string",
+        "type": 0,
+        "houseId": 1
+      },
+      {
+        "name": "r2",
+        "description": "string",
+        "type": 0,
+        "houseId": 1
+      }
+    ], function(err, room){
+      if (er) throw er;
+      Resident.create([
+        {
+          "name": "resident 1",
+          "age": 0,
+          "gender": 0,
+          "location": "1",
+          "income": 0,
+          "userId": 0
+        },
+        {
+          "name": "resident 2",
+          "age": 0,
+          "gender": 1,
+          "location": "1",
+          "income": 0,
+          "userId": 0
+        },
+      ],function(err, residents){
+        console.log("created");
+        if (er) throw er;
+        console.log(houses[0]);
+        ds.disconnect();
+      })
+    });
+  });
+
+});

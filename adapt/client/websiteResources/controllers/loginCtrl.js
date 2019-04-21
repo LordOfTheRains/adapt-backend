147f8c22c9784f21b63924a574e3801e5356948b
jvandeventer <jvandeventer@172.19.65.59>
2019-04-21 08:08:47 -0500

    Logout should now work.

--- adapt/client/websiteResources/controllers/loginCtrl.js
+++ adapt/client/websiteResources/controllers/loginCtrl.js
@@ -15,17 +15,30 @@ app.controller('loginCtrl', function($scope, $http, $location, $rootScope, login
     var res = $http.post(serverURL + 'api/Users/login', dataObj);
     res.then(function(data, status, headers, config) {
       alert("Login Worked");
+      var response = data;
+      var accesstoken = response.data.id;
+      sessionStorage.setItem("user", $scope.email);
+      sessionStorage.setItem("accesstoken", accesstoken);
       loginService.setPermission("loggedIn", true);
       $location.path('/recommendations');
       $rootScope.email = $scope.email;
-      sessionStorage.setItem("accessToken", data.id)
-      sessionStorage.setItem("user", $scope.email);
     }, function(data, status, headers, config) {
       alert( "failure message: " + JSON.stringify({data: data}));
     });
   };
 
+  //Logs the user out and resets permissions and rootScope variables/sessionStorage
   $scope.logout = function(){
-
+    var accesstoken = sessionStorage.getItem("accesstoken");
+    var res = $http.post(serverURL + "api/Users/logout?access_token=" + accesstoken);
+    res.then(function(data, status, headers, config) {
+      alert("Logging out user.")
+      loginService.setPermission("loggedIn", false);
+      $rootScope.email = "";
+      sessionStorage.clear();
+      $location.path("/");
+    }, function(data, status, headers, config) {
+      alert( "failure message: " + JSON.stringify({data: data}));
+    });
   };
 });

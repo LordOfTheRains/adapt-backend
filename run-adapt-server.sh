ce45f5129026ba37158d2437ca937ed49cd03941
Rain Li <yrl0001@auburn.edu>
2019-04-22 00:39:06 -0500

    updated volume path

--- run-adapt-server.sh
+++ run-adapt-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker rm -f adapt-backend
-docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend adapt-server \
--v $PWD/adapt/server/storage:/data/app/adapt-backend/adapt/server/storage
+docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend adapt-server -v $PWD/../adapt-image-server:/data/app/adapt-backend/adapt/server/storage
 

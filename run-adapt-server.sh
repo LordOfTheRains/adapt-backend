b601cab6bf10b5bbb10e0f1d7be433557db62e1a
Rain Li <yrl0001@auburn.edu>
2019-04-22 00:43:15 -0500

    changed the order of the mount command

--- run-adapt-server.sh
+++ run-adapt-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker rm -f adapt-backend
-docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend adapt-server -v $PWD/../adapt-image-server:/data/app/adapt-backend/adapt/server/storage
+docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend \
+-v $PWD/../adapt-image-server:/data/app/adapt-backend/adapt/server/storage \
+adapt-server 
 

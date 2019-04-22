93a4e1658bba318187a6dca914e44d472b3700b5
Rain Li <yrl0001@auburn.edu>
2019-04-22 00:29:50 -0500

    added volume mount for image directory

--- run-adapt-server.sh
+++ run-adapt-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker rm -f adapt-backend
-docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend adapt-server
+docker run --network=adapt-darknet --ip 172.19.0.21 -it -d -p 8080:3000 --name=adapt-backend adapt-server \
+-v $PWD/adapt/server/storage:/data/app/adapt-backend/adapt/server/storage
+
